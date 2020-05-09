40c9962d89c439af99794fbc6df4ac563ab328b1
Travis Mathis <travisdmathis@gmail.com>
2020-05-09 14:17:53 -0400

    adds eternal storage location formatting helper

--- contracts/lib/EternalStorageHelper.sol
+++ contracts/lib/EternalStorageHelper.sol
@@ -2,14 +2,7 @@ pragma solidity ^0.6.0;
 
 
 library EternalStorageHelper {
-    function formatString(string memory _storageLocation, string memory _value)
-        internal
-        pure
-        returns (bytes32)
-    {
-        return keccak256(abi.encodePacked(_storageLocation, _value));
-    }
-
+    // Setter Format
     function formatAddress(string memory _storageLocation, address _value)
         internal
         pure
@@ -18,6 +11,39 @@ library EternalStorageHelper {
         return keccak256(abi.encodePacked(_storageLocation, _value));
     }
 
+    function formatBool(string memory _storageLocation, bool _value)
+        internal
+        pure
+        returns (bytes32)
+    {
+        return keccak256(abi.encodePacked(_storageLocation, _value));
+    }
+
+    function formatInt(string memory _storageLocation, int256 _value)
+        internal
+        pure
+        returns (bytes32)
+    {
+        return keccak256(abi.encodePacked(_storageLocation, _value));
+    }
+
+    function formatString(string memory _storageLocation, string memory _value)
+        internal
+        pure
+        returns (bytes32)
+    {
+        return keccak256(abi.encodePacked(_storageLocation, _value));
+    }
+
+    function formatUint(string memory _storageLocation, uint256 _value)
+        internal
+        pure
+        returns (bytes32)
+    {
+        return keccak256(abi.encodePacked(_storageLocation, _value));
+    }
+
+    // Getter Format
     function formatGet(string memory _location)
         internal
         pure

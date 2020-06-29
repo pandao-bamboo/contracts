1186a507072b4bae3935175490f755ca95d899bc
Travis Mathis <travisdmathis@gmail.com>
2020-06-29 09:54:42 -0400

    WIP

--- contracts/lib/StringHelper.sol
+++ contracts/lib/StringHelper.sol
@@ -1,8 +1,12 @@
 pragma solidity 0.6.6;
 
-
 library StringHelper {
   function concat(string memory a, string memory b) internal pure returns (string memory) {
     return string(abi.encodePacked(a, b));
+  }
+
+  /// @notice convert address to string
+  function toString(address account) internal pure returns (string memory) {
+    return string(abi.encodePacked(account));
   }
 }
